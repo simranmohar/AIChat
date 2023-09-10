49b2b7ae12ac24bee0a9d2d9a53e09b4349feb1a
Simran Mohar <77524871+simranmohar@users.noreply.github.com>
2023-09-09 19:16:41 -0700

    create chat header

---
+++ client/src/components/chat/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from "react-chat-engine-advanced";
+import Header from "@/components/customHeader";
+function Chat() {
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "testuser", // username
+    "testuser" // password
+  );
+
+  return (
+    <div style={{ flexBasis: "100%" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: "100vh" }}
+        renderChatHeader={(chat) => <Header chat={chat} />}
+      />
+    </div>
+  );
+}
+
+export default Chat;
---
+++ client/src/components/customHeader/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid";
+
+function CustomHeader() {
+  return (
+    <div className="chat-header">
+      <div></div>
+    </div>
+  );
+}
+
+export default CustomHeader;
