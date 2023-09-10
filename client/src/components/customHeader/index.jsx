9299cdd7bfc910443eb17e226e39eff1b953e843
Simran Mohar <77524871+simranmohar@users.noreply.github.com>
2023-09-10 15:38:57 -0700

    fix custom header

--- client/src/components/customHeader/index.jsx
+++ client/src/components/customHeader/index.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { ChatBubbleLeftRightIcon, PhoneIcon } from "@heroicons/react/24/solid";
 
-function CustomHeader() {
+const CustomerHeader = ({ chat }) => {
   return (
     <div className="chat-header">
-      <div></div>
+      <div className="flexbetween">
+        <ChatBubbleLeftRightIcon className="icon-chat" />
+        <h3 className="header-text">{chat.title}</h3>
+      </div>
+      <div className="flexbetween">
+        <PhoneIcon className="icon-phone" />
+        {chat.description !== "⬅️ ⬅️ ⬅️" ? (
+          <p className="header-text">{chat.description}</p>
+        ) : (
+          <p className="header-text">no chat selected</p>
+        )}
+      </div>
     </div>
   );
-}
+};
 
-export default CustomHeader;
+export default CustomerHeader;
